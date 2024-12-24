66e9377d37fb84e73622357bc09de2584c54b00b
Tran Van Minh Tri <tvmthtr@gmail.com>
2024-12-24 19:33:41 +0700

    update

--- ytfzf/conf.sh
+++ ytfzf/conf.sh
@@ -3,10 +3,10 @@ YTFZF_LOOP=1 # if set to 1 it is on but normally it is off by default. Can be tu
 THUMBNAILS_VIEWER=kitty
 SHOW_THUMBNAILS=1
 ASYNC_THUMBNAILS=1
-YTFZF_PREF="bestvideo[height<=?1080]+bestaudio/best" # set the video format
-YTFZF_ENABLE_FZF_DEFAULT_OPTS=1                      # fzf colors are going to be the one from your fzf configuration
-FZF_PLAYER="mpv"                                     # sets the video player used by ytfzf (mpv by default), e.g. FZF_PLAYER="devour mpv"; you can also specify the YTFZF_PLAYER_FORMAT, e.g. YTFZF_PLAYER_FORMAT="devour mpv --ytdl-format="
+# YTFZF_PREF="bestvideo[height<=?1080]+bestaudio/best" # set the video format
+YTFZF_ENABLE_FZF_DEFAULT_OPTS=1 # fzf colors are going to be the one from your fzf configuration
+FZF_PLAYER="mpv"                # sets the video player used by ytfzf (mpv by default), e.g. FZF_PLAYER="devour mpv"; you can also specify the YTFZF_PLAYER_FORMAT, e.g. YTFZF_PLAYER_FORMAT="devour mpv --ytdl-format="
 SCRAPE="youtube"
 SEARCH_RESULT_TYPE='all'
-YTDL_OPTS="--config-location ~/.config/yt-dlp/config"
+# YTDL_OPTS="--config-location ~/.config/yt-dlp/config"
 NFSW=true
